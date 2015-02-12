bda121b87a202246a64ae00f78331c1f6313aa16
Sergey Kamardin <gobwas@gmail.com>
2015-02-13 00:56:07 +0300

    kill ie8

--- .karma.js
+++ .karma.js
@@ -23,12 +23,6 @@ module.exports = function(config) {
       platform: 'OS X 10.9',
       version: '7.1'
     },
-    sl_ie_8: {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows XP',
-      version: '8'
-    },
     sl_ie_9: {
       base: 'SauceLabs',
       browserName: 'internet explorer',
