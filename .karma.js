ddb8990256566fd8f7b0a41016d8f45e97587b5a
Sergey Kamardin <gobwas@gmail.com>
2015-02-13 17:33:25 +0300

    karma tune

--- .karma.js
+++ .karma.js
@@ -81,7 +81,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'saucelabs'],
+    reporters: ['saucelabs'],
 
     // web server port
     port: 9876,
